413ef0ffdebd62e6c86f0e2e85e2eede070a8368
estudantesilva <104015001+estudantesilva@users.noreply.github.com>
2022-11-14 21:44:22 -0300

    Concluído o módulo 9 - Aceitando convites e exibindo nossos contatos

--- experiencein/perfis/urls.py
+++ experiencein/perfis/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from perfis import views
 
 urlpatterns = [
-	path('', views.index, name='index'),
-	path('perfis/<int:perfil_id>', views.exibir, name='exibir'),
-	path('perfis/<int:perfil_id>/convidar', views.convidar, name='convidar')
+ path('', views.index, name='index'),
+ path('perfis/<int:perfil_id>', views.exibir, name='exibir'),
+ path('perfis/<int:perfil_id>/convidar', views.convidar, name='convidar'),
+ path('convite/<int:convite_id>/aceitar', views.aceitar, name='aceitar')
 ]
