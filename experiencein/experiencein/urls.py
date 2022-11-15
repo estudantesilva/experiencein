8fd80ff34fe4a4c63e35001e3db36f038ed82eda
estudantesilva <104015001+estudantesilva@users.noreply.github.com>
2022-11-15 14:54:11 -0300

    Concluído o módulo 10 - Cadastro de usuários

--- experiencein/experiencein/urls.py
+++ experiencein/experiencein/urls.py
@@ -17,6 +17,7 @@ from django.urls import include, path
 from django.contrib import admin
 
 urlpatterns = [
-    path('admin/',admin.site.urls), 
-    path('', include('perfis.urls'))
+ path('admin/', admin.site.urls),
+ path('', include('perfis.urls')),
+ path('', include('usuarios.urls'))
 ]
